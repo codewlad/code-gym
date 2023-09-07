8e643ac0f477435c4182a4990532adc7eebd4835
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-09-07 19:10:02 -0300

    Added Axios, AppError and Interceptors

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,9 +1,21 @@
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import {
+	VStack,
+	Image,
+	Text,
+	Center,
+	Heading,
+	ScrollView,
+	useToast,
+} from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import * as yup from 'yup';
+
+import { api } from '@services/api';
+
+import { AppError } from '@utils/AppError';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -31,6 +43,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+	const toast = useToast();
+
 	const {
 		control,
 		handleSubmit,
@@ -45,23 +59,26 @@ export function SignUp() {
 		navigation.goBack();
 	}
 
-	async function handleSignUp({
-		name,
-		email,
-		password,
-		password_confirm,
-	}: FormDataProps) {
-		const response = await fetch('http://192.168.0.80:3333/users', {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ name, email, password }),
-		});
+	async function handleSignUp({ name, email, password }: FormDataProps) {
+		try {
+			const response = await api.post('/users', {
+				name,
+				email,
+				password,
+			});
+			console.log(response.data);
+		} catch (error) {
+			const isAppError = error instanceof AppError;
+			const title = isAppError
+				? error.message
+				: 'Não foi possível criar a conta. Tente novamente mais tarde.';
 
-		const data = await response.json();
-		console.log(data);
+			toast.show({
+				title,
+				placement: 'top',
+				bgColor: 'red.500',
+			});
+		}
 	}
 
 	return (
