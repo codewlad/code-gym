82c8623f3f4fb0bfbe3cce040ba8dcca1086a1ba
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-09-09 11:50:03 -0300

    Context added

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, createContext } from 'react';
+
+import { UserDTO } from '@dtos/UserDTO';
+
+export type AuthContextDataProps = {
+	user: UserDTO;
+};
+
+type AuthContextProviderProps = {
+	children: ReactNode;
+};
+
+export const AuthContext = createContext<AuthContextDataProps>(
+	{} as AuthContextDataProps
+);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+	return (
+		<AuthContext.Provider
+			value={{
+				user: {
+					id: '1',
+					name: 'Wladimir',
+					email: 'wlad@email.com',
+					avatar: 'wladimir.png',
+				},
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+}
